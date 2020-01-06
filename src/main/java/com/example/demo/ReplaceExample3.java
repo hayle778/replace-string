8bdccf75d3f1bfb52bbc1c477ff539a827786d06
yourUsername <yourEmail@domain.com>
2020-01-05 23:37:47 -0500

    rs

---
+++ src/main/java/com/example/demo/ReplaceExample3.java
@@ -0,0 +1,32 @@
+package com.example.demo;
+
+
+
+public class ReplaceExample3 {
+        static public void main(String[] args) {
+            String str = "oooooo-hhhh-oooooo";
+           String rs = str.replace("h","s"); // Replace 'h' with 's'
+            System.out.println(rs);
+            rs = rs.replace("s","h"); // Replace 's' with 'h'
+            System.out.println(rs);
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
